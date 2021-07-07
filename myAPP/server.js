3e659bbbc05b4d25638c809f837dcf44621c2394
Sridhar Kidambi <sridharhere@gmail.com>
2021-07-07 23:13:21 +0530

    first commit

---
+++ myAPP/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+  sampleconfig="configmap value";
+  samplesecret="secret value";
+    data= " I am configmap value :" + sampleconfig +" I M  secret value:" + samplesecret +"Secret value name " + process.env.sridhar+"Secret value lname " + process.env.kidambi;
+   res.send(data);
+//    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+//       console.log( data );
+//       res.end( data );
+//    });
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
