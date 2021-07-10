1ea30263cf3d6ae2a47967fdcc07663ab14a9414
Sridhar Kidambi <sridharhere@gmail.com>
2021-07-10 22:26:49 +0530

    secret password

--- myAPP/server.js
+++ myAPP/server.js
@@ -5,7 +5,7 @@ var fs = require("fs");
 app.get('/listUsers', function (req, res) {
   sampleconfig="configmap value";
   samplesecret="secret value";
-    data= " I am configmap value :" + sampleconfig +" I M  secret value:" + samplesecret +"Secret value name " + process.env.sridhar+"Secret value lname " + process.env.kidambi;
+    data= " I am configmap value :" + sampleconfig +" I M  secret value:" + samplesecret +" , Secret value name " + process.env.sridhar + " , Secret value lname " + process.env.kidambi+ " , secret password is:"+ process.env.SECRET_PWD;
    res.send(data);
 //    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
 //       console.log( data );
